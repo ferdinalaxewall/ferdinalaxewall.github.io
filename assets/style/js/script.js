268f4a9821a635c72da9835e27ad9aedd09a3446
ferdinalaxe <ferdinalraihan@gmail.com>
2022-09-15 10:04:30 +0700

    fix bug in big viewport (higher than 1440px)

--- assets/style/js/script.js
+++ assets/style/js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    console.log("ok")
+    $(".skill-button").click(function(){
+        let skillAttr = $(this).attr("skill-attribute");
+
+        $(".skill-wrapper img").each(function(){
+            if ($(this).attr("skill-attribute") == skillAttr) {
+                $(this).fadeIn();
+            }else{
+                $(this).fadeOut();
+            }
+        })
+
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(".skill-wrapper img").fadeIn();
+        }else{
+            $(".skill-button").removeClass("active")
+            $(this).addClass("active")
+        }
+    })
 });
